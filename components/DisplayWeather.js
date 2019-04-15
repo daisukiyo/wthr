59adbe271e4818768b58d32fef8be415ad1a4c60
daisukiyo <tk.rain09@gmail.com>
2019-04-15 00:47:50 -0700

    added displayweather component

---
+++ components/DisplayWeather.js
@@ -0,0 +1,30 @@
+// this is a simple component to display the weather
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+// this component is defined as a function
+const DisplayWeather = (props) => {
+    // component must return JSX
+    return (
+        // view container will act as a component
+        <View>
+            {/* text will display the temperature */}
+            <Text>72˚</Text>
+        </View>
+    )
+}
+
+export default DisplayWeather
+
+// add a stylesheet by defining a variable called 'styles'
+const styles = StyleSheet.create({
+    // assigning an object with CSS properties
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    temp: {
+        fontSize: 40,
+        fontWeight: 'bold',
+    }
+})
